fbc7a112fb49e1b8c007524cebc2337c9e8238c7
Carlos Garcia <cgarciapieto@gmail.com>
2018-07-24 13:53:06 -0500

    java-array-classwork1-cgarciapieto.iml

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        
 
+
+        ArrayList<String> nameArray = new ArrayList<String>();
+        nameArray.add("Bob");
+        nameArray.add("John");
+        nameArray.add("Carlos");
+        nameArray.add("Mike");
+
+        System.out.println(nameArray);
     }
 }
---
+++ src/excercise24.java
@@ -0,0 +1,19 @@
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class excercise24 {
+    public static void main(String[] args) {
+        // ArrayList<String> squad = new ArrayList<String>();
+        ArrayList<Integer> numberArray = new ArrayList<Integer>();
+        numberArray.add(1);
+        numberArray.add(2);
+        numberArray.add(3);
+        numberArray.add(4);
+        numberArray.add(5);
+        System.out.println(numberArray.get(1));
+        {
+        }
+
+    }
+}
